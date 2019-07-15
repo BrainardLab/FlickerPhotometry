b8e45011b053d775ad123559d331dac60a3c7d81
David Brainard <brainard@psych.upenn.edu>
2019-07-15 13:12:03 -0400

    Convert to project

---
+++ configuration/FlickerPhotometryLocalHookTemplate.m
@@ -0,0 +1,50 @@
+function FlickerPhotometryLocalHook
+% FlickerPhotometryLocalHook
+%
+% Configure things for working on the FlickerPhotometry project.
+%
+% For use with the ToolboxToolbox.  If you copy this into your
+% ToolboxToolbox localToolboxHooks directory (by defalut,
+% ~/localToolboxHooks) and delete "LocalHooksTemplate" from the filename,
+% this will get run when you execute tbUseProject('FlickerPhotometry') to set up for
+% this project.  You then edit your local copy to match your configuration.
+%
+% You will need to edit the project location and i/o directory locations
+% to match what is true on your computer.
+
+%% Say hello
+fprintf('Running FlickerPhotometry local hook\n');
+
+%% Specify project name and location
+projectName = 'FlickerPhotometry';
+projectBaseDir = tbLocateProject(projectName);
+if (ispref(projectName))
+    rmpref(projectName);
+end
+
+% If we ever needed some user/machine specific preferences, this is one way
+% we could do that.
+sysInfo = GetComputerInfo();
+switch (sysInfo.localHostName)
+    case 'eagleray'
+        % DHB's desktop
+        baseDir = fullfile(filesep,'Volumes','Users1','Dropbox (Aguirre-Brainard Lab)');
+ 
+    otherwise
+        % Some unspecified machine, try user specific customization
+        switch(sysInfo.userShortName)
+            % Could put user specific things in, but at the moment generic
+            % is good enough.
+            otherwise
+                baseDir = fullfile('/Users/',sysInfo.userShortName,'Dropbox (Aguirre-Brainard Lab)');
+        end
+end
+
+%% Set preferences for project output
+%
+% This will need to be locally configured.
+outputBaseDir = fullfile(baseDir,'CNST_data','FlickerPhotometry');
+
+% Set the preferences
+setpref(projectName,'outputBaseDir',outputBaseDir);
+
